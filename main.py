bc85367239188e2257d5a22278287521f7281fc9
IceKyrin <40847087+IceKyrin@users.noreply.github.com>
2022-09-29 10:06:58 +0800

    update

---
+++ main.py
@@ -0,0 +1,112 @@
+import os
+import json
+import utils
+import torch
+import hubert
+import shutil
+import logging
+import demjson
+import soundfile
+import infer_tool
+import torchaudio
+import numpy as np
+from wav_temp import merge
+from models import SynthesizerTrn
+import icassp2022_vocal_transcription
+
+logging.getLogger('numba').setLevel(logging.WARNING)
+
+
+def get_units(path):
+    source, sr = torchaudio.load(path)
+    source = torchaudio.functional.resample(source, sr, 16000)
+    source = source.unsqueeze(0)
+    with torch.inference_mode():
+        units = hubert_soft.units(source)
+        return units
+
+
+# 此版本要求numpy<=1.19.2
+# 此版本如使用cuda加速，自行安装cuda、cudnn等，需6g显存以上（脚本自动检测是否支持cuda）
+
+# 可填写音源文件列表，音源文件格式为单声道22050采样率wav，放置于raw文件夹下
+clean_names = ["多声线测试"]
+# bgm、trans分别对应歌曲列表，若能找到相应文件、则自动合并伴奏，若找不到bgm，则输出干声（不使用bgm合成多首歌时，可只随意填写一个不存在的bgm名）
+bgm_names = ["bgm1"]
+# 合成多少歌曲时，若半音数量不足、自动补齐相同数量（按第一首歌的半音）
+trans = [-3]  # 加减半音数（可为正负）
+# 每首歌同时输出的speaker_id
+id_list = [2]
+
+# 每次合成长度，建议30s内，太高了爆掉显存(gtx1066一次15s以内）
+cut_time = 15
+model_name = "697_epochs"  # 模型名称（pth文件夹下）
+config_name = "yilanqiu.json"  # 模型配置（config文件夹下）
+
+# 自行下载hubert-soft-0d54a1f4.pt改名为hubert.pt放置于pth文件夹下
+# https://github.com/bshall/hubert/releases/tag/v0.1
+hubert_soft = hubert.hubert_soft('pth/hubert.pt')
+
+# 以下内容无需修改
+hps_ms = utils.get_hparams_from_file(f"configs/{config_name}")
+dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+net_g_ms = SynthesizerTrn(
+    178,
+    hps_ms.data.filter_length // 2 + 1,
+    hps_ms.train.segment_size // hps_ms.data.hop_length,
+    n_speakers=hps_ms.data.n_speakers,
+    **hps_ms.model)
+_ = utils.load_checkpoint(f"pth/{model_name}.pth", net_g_ms, None)
+_ = net_g_ms.eval().to(dev)
+
+# 自动补齐
+infer_tool.fill_a_to_b(bgm_names, clean_names)
+infer_tool.fill_a_to_b(trans, clean_names)
+for clean_name, bgm_name, tran in zip(clean_names, bgm_names, trans):
+    infer_tool.resample_to_22050(f'./raw/{clean_name}.wav')
+    for speaker_id in id_list:
+        speakers = demjson.decode_file(f"configs/{config_name}")["speakers"]
+        out_audio_name = model_name + f"_{clean_name}_{speakers[speaker_id]}"
+        # 清除缓存文件
+        infer_tool.del_file("./wav_temp/input/")
+        infer_tool.del_file("./wav_temp/output/")
+
+        raw_audio_path = f"./raw/{clean_name}.wav"
+        audio, sample_rate = torchaudio.load(raw_audio_path)
+
+        audio_time = audio.shape[-1] / 22050
+        if audio_time > 1.3 * int(cut_time):
+            infer_tool.cut(int(cut_time), raw_audio_path, out_audio_name, "./wav_temp/input")
+        else:
+            shutil.copy(f"./raw/{clean_name}.wav", f"./wav_temp/input/{out_audio_name}-0.wav")
+        file_list = os.listdir("./wav_temp/input")
+
+        count = 0
+        for file_name in file_list:
+            source_path = "./wav_temp/input/" + file_name
+            audio, sample_rate = torchaudio.load(source_path)
+            input_size = audio.shape[-1]
+
+            sid = torch.LongTensor([int(speaker_id)]).to(dev)
+            soft = get_units(source_path).squeeze(0).numpy()
+            pitch = icassp2022_vocal_transcription.transcribe(source_path)
+            pitch[pitch != 0] += tran
+            if tran == 100:
+                pitch[:] = 0
+            pitch = infer_tool.resize2d_plus(pitch, len(soft[:, 0]))
+            pitch = torch.LongTensor(pitch).unsqueeze(0).to(dev)
+            stn_tst = torch.FloatTensor(soft)
+
+            with torch.no_grad():
+                x_tst = stn_tst.unsqueeze(0).to(dev)
+                x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).to(dev)
+                audio = \
+                    net_g_ms.infer(x_tst, x_tst_lengths, pitch, sid=sid, noise_scale=.1, noise_scale_w=0.5,
+                                   length_scale=1)[0][
+                        0, 0].data.float().cpu().numpy()
+
+            soundfile.write("./wav_temp/output/" + file_name, audio, int(audio.shape[0] / input_size * 22050))
+            count += 1
+            print("%s success: %.2f%%" % (file_name, 100 * count / len(file_list)))
+        merge.run(out_audio_name, bgm_name, out_audio_name)
